2fbb7c17dd6dc6cdfa85316284b93eff90a21850
warleymartinsv@gmail.com <warleymartinsv@gmail.com>
2020-08-26 11:46:24 -0300

    cadastrar, remover e alterar

--- src/Estacionamento_banco_de_dados/Program.cs
+++ src/Estacionamento_banco_de_dados/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net.Http.Headers;
 
@@ -13,8 +14,11 @@ namespace Estacionamento_banco_de_dados
             int opcaoMenu;
             do
             {
-                PrintarMenu();
-                opcaoMenu = int.Parse(Console.ReadLine());
+                do
+                {
+                    PrintarMenu();
+                    opcaoMenu = int.Parse(Console.ReadLine());
+                } while (opcaoMenu < 0 || opcaoMenu > 3);
                 using (var contexto = new EstacionamentoContext())
                 {
                     switch (opcaoMenu)
@@ -23,12 +27,54 @@ namespace Estacionamento_banco_de_dados
                             CadastrarCliente(contexto);
                             break;
                         case 2:
-                            RemoverCliente(contexto);;
+                            RemoverCliente(contexto); ;
+                            break;
+                        case 3:
+                            AlterarDados(contexto);
                             break;
                     }
                 }
             } while (opcaoMenu != 0);
 
+        }
+
+        private static void AlterarDados(EstacionamentoContext contexto)
+        {
+            Console.Write("Digite o cpf do cliente que deseja alterar: ");
+            string cpf = Console.ReadLine();
+            var cliente = contexto.Clientes.Where(c => c.CPF.Equals(cpf)).FirstOrDefault();
+            int opcaoMenu;
+            do
+            {
+                do
+                {
+                    Console.Write("\nDeseja alterar: " +
+                                  "\n(1). Nome" +
+                                  "\n(2). CPF" +
+                                  "\n(3). Veiculo" +
+                                  "\n(0). Salvar alterações" +
+                                  "\nOpção: ");
+                    opcaoMenu = int.Parse(Console.ReadLine());
+                } while (opcaoMenu < 0 || opcaoMenu > 3);
+
+                switch (opcaoMenu)
+                {
+                    case 1:
+                        Console.Write("Digite o novo nome: ");
+                        cliente.Nome = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Digite o novo cpf: ");
+                        cliente.CPF = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Digite a nova placa: ");
+                        cliente.PlacaCarro = Console.ReadLine();
+                        break;
+                }
+            } while (opcaoMenu != 0);
+            contexto.Clientes.AddOrUpdate(cliente);
+            contexto.SaveChanges();
         }
 
         private static void Entries(IEnumerable<DbEntityEntry> entries)
@@ -50,7 +96,7 @@ namespace Estacionamento_banco_de_dados
 
             Console.Write("Digite o nome do cliente: ");
             string nome = Console.ReadLine();
-            Console.Write("Digite a idade do cliente: ");
+            Console.Write("Digite o cpf do cliente: ");
             string cpf = Console.ReadLine();
             Console.Write("Digite a placa do veiculo do cliente: ");
             string placa = Console.ReadLine();
@@ -63,6 +109,7 @@ namespace Estacionamento_banco_de_dados
             Console.Write("\nDigite a opção desejada:" +
                           "\n(1). Cadastrar um novo cliente" +
                           "\n(2). Remover um cliente" +
+                          "\n(3). Alterar dados de um cliente" +
                           "\n(0). Encerrar" +
                           "\nOpção: ");
         }
